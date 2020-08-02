29484e9f348aa068c663eb004be5b79a3e65d036
Theodor Constantin <theodorcon@gmail.com>
2020-08-02 14:33:35 +0200

    Finished meta events

--- MIDIMan.hs
+++ MIDIMan.hs
@@ -47,62 +47,11 @@ data MetaEvent = SeqNum Int -- FF 00 02 ss ss
                | TrackEnd --FF 2F 00
                | Tempo Int --FF 51 03 tt tt tt
                | SMTPEOffset Int Int Int Int Float --FF 54 05 hh mm ss fr ff
-               | TimeSign Int Int Int Int --
-               | KeySign Int Bool --
-               | SeqSpec Int String String --
+               | TimeSign Int Int Int Int --FF 58 04 nn dd cc bb
+               | KeySign Int Bool --FF 59 02 sf mi
+               | SeqSpec String String --FF 7F <len> <id> <data>
 
-bsSeqNum :: BSParser MetaEvent 
-bsSeqNum = (SeqNum . strToInt) <$> (bsHexString "FF 00 02" *> bsMultiChar 2)
-
-bsText :: BSParser MetaEvent
-bsText = Text <$> (bsHexString "FF 01" *> bsVarStr 1) 
-
-bsCopyright :: BSParser MetaEvent
-bsCopyright = Copyright <$> (bsHexString "FF 02" *> bsVarStr 1) 
-
-bsSeqName :: BSParser MetaEvent
-bsSeqName = SeqName <$> (bsHexString "FF 03" *> bsVarStr 1) 
-
-bsInstrument :: BSParser MetaEvent
-bsInstrument = Instrument <$> (bsHexString "FF 04" *> bsVarStr 1) 
-
-bsLyric :: BSParser MetaEvent
-bsLyric = Lyric <$> (bsHexString "FF 05" *> bsVarStr 1) 
-
-bsMarker :: BSParser MetaEvent
-bsMarker = Marker <$> (bsHexString "FF 06" *> bsVarStr 1) 
-
-bsCuePoint :: BSParser MetaEvent
-bsCuePoint = CuePoint <$> (bsHexString "FF 07" *> bsVarStr 1) 
-
-bsChanPref :: BSParser MetaEvent
-bsChanPref = ChanPref <$> (bsHexString "FF 20 01" *> bsInt 1)
-
-bsTrackEnd :: BSParser MetaEvent
-bsTrackEnd = (const TrackEnd) <$> bsHexString "FF 2F 00" 
-
-bsTempo :: BSParser MetaEvent
-bsTempo = Tempo <$> (bsHexString "FF 51 03" *> bsInt 1)
-
-bsSMTPE :: BSParser MetaEvent
-bsSMTPE = SMTPEOffset <$> (bsHexString "FF 54 05" *> bsInt 1) 
-                      <*> bsInt 1
-                      <*> bsInt 1
-                      <*> bsInt 1
-                      <*> ((\i -> (fromIntegral i) / 100) <$> bsInt 1)
-
-bsVarStr :: Int -> BSParser String 
-bsVarStr lenChars = BSParser prs where
-    prs str = let lengthResult = run (bsMultiChar lenChars) str in
-        case lengthResult of
-            Nothing -> Nothing
-            Just (lengthString, rest) -> let length = strToInt lengthString in
-                run (bsMultiChar length) rest
-
-bsInt :: Int -> BSParser Int
-bsInt len = strToInt <$> bsMultiChar len
-
-data ChannelVoiceEvent = NoteOff Int Int Int
+data ChannelVoiceEvent = NoteOff Int Int Int -- 
                        | PolyKeyPress Int Int Int
                        | CtrlChange Int Int Int
                        | PrgChange Int Int Int
@@ -173,6 +122,17 @@ bsString = sequenceA . map bsChar
 bsHexString :: String -> BSParser String
 bsHexString hex = bsString $ hexStringToCharString hex
 
+bsVarStr :: Int -> BSParser String 
+bsVarStr lenChars = BSParser prs where
+    prs str = let lengthResult = run (bsMultiChar lenChars) str in
+        case lengthResult of
+            Nothing -> Nothing
+            Just (lengthString, rest) -> let length = strToInt lengthString in
+                run (bsMultiChar length) rest
+
+bsInt :: Int -> BSParser Int
+bsInt len = strToInt <$> bsMultiChar len
+
 bsMThd :: BSParser MThdChunk
 bsMThd = func <$> (bsString "MThd" 
      *> (bsMultiChar 4))
@@ -204,6 +164,8 @@ bsMTrk = func <$> (bsString "MTrk"
                             then (acc ++ [x], xs)
                             else splitString xs (acc ++ [x])
 
+-- Sysex Events
+
 bsSysex :: BSParser SysexEvent
 bsSysex = F0 <$> (bsHexString "F0" *> BSParser prs) 
       <|> F7 <$> (bsHexString "F7" *> BSParser prs) where 
@@ -211,6 +173,83 @@ bsSysex = F0 <$> (bsHexString "F0" *> BSParser prs)
             then Nothing
             else let Just (strInt, rest) = run bsAnyChar str in
                 run (bsMultiChar (ord strInt)) rest
+
+-- Meta Events
+
+bsMeta :: BSParser MetaEvent
+bsMeta = bsSeqNum
+     <|> bsText
+     <|> bsCopyright
+     <|> bsSeqName
+     <|> bsInstrument
+     <|> bsLyric
+     <|> bsMarker
+     <|> bsCuePoint
+     <|> bsChanPref
+     <|> bsTrackEnd
+     <|> bsTempo
+     <|> bsSMTPE
+     <|> bsTimeSign
+     <|> bsKeySign
+     <|> bsSeqSpec
+
+bsSeqNum :: BSParser MetaEvent 
+bsSeqNum = (SeqNum . strToInt) <$> (bsHexString "FF 00 02" *> bsMultiChar 2)
+
+bsText :: BSParser MetaEvent
+bsText = Text <$> (bsHexString "FF 01" *> bsVarStr 1) 
+
+bsCopyright :: BSParser MetaEvent
+bsCopyright = Copyright <$> (bsHexString "FF 02" *> bsVarStr 1) 
+
+bsSeqName :: BSParser MetaEvent
+bsSeqName = SeqName <$> (bsHexString "FF 03" *> bsVarStr 1) 
+
+bsInstrument :: BSParser MetaEvent
+bsInstrument = Instrument <$> (bsHexString "FF 04" *> bsVarStr 1) 
+
+bsLyric :: BSParser MetaEvent
+bsLyric = Lyric <$> (bsHexString "FF 05" *> bsVarStr 1) 
+
+bsMarker :: BSParser MetaEvent
+bsMarker = Marker <$> (bsHexString "FF 06" *> bsVarStr 1) 
+
+bsCuePoint :: BSParser MetaEvent
+bsCuePoint = CuePoint <$> (bsHexString "FF 07" *> bsVarStr 1) 
+
+bsChanPref :: BSParser MetaEvent
+bsChanPref = ChanPref <$> (bsHexString "FF 20 01" *> bsInt 1)
+
+bsTrackEnd :: BSParser MetaEvent
+bsTrackEnd = (const TrackEnd) <$> bsHexString "FF 2F 00" 
+
+bsTempo :: BSParser MetaEvent
+bsTempo = Tempo <$> (bsHexString "FF 51 03" *> bsInt 1)
+
+bsSMTPE :: BSParser MetaEvent
+bsSMTPE = SMTPEOffset <$> (bsHexString "FF 54 05" *> bsInt 1) 
+                      <*> bsInt 1
+                      <*> bsInt 1
+                      <*> bsInt 1
+                      <*> ((\i -> (fromIntegral i) / 100) <$> bsInt 1)
+
+bsTimeSign :: BSParser MetaEvent
+bsTimeSign = TimeSign <$> (bsHexString "FF 58 04" *> bsInt 1) 
+                      <*> bsInt 1
+                      <*> bsInt 1
+                      <*> bsInt 1
+
+bsKeySign :: BSParser MetaEvent
+bsKeySign = KeySign <$> (bsHexString "FF 59 02" *> bsInt 1)
+                    <*> (toBool <$> bsInt 1) where
+    toBool int = case int of
+        0 -> False
+        1 -> True
+        _ -> error $ "The second number in Key Signature is not a boolean: " ++ show int
+
+bsSeqSpec :: BSParser MetaEvent
+bsSeqSpec = toSeqSpec <$> (bsHexString "FF 7F" *> bsVarStr 1) where
+    toSeqSpec (x:xs) = SeqSpec [x] xs
 
 bsMIDI :: BSParser MIDI
 bsMIDI = func <$> bsMThd <*> (repeatParser bsMTrk) where
